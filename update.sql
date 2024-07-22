4774e4bd64c631a6cce00041589760c6562675f2
Joaquin Albors <joaquin.albors01@gmail.com>
2024-07-23 00:22:10 +0200

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,4 +1,11 @@
 -- Active: 1721290976752@@127.0.0.1@3306@dealership
-INSERT INTO customers (name, email) VALUES("Pablo Picasso", "ppicasso@gmail.com");
-INSERT INTO customers (name, email) VALUES("Abraham Lincoln", "lincoln@us.gov");
-INSERT INTO customers (name, email) VALUES("Napoléon Bonaparte", "hello@napoleon.me")
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE customers
+SET cust_email = 'ppicasso@gmail.com' WHERE cust_id = 10001;
+
+UPDATE customers
+SET cust_email = 'lincoln@us.gov' WHERE cust_id = 20001;
+
+UPDATE customers
+SET cust_email = 'hello@napoleon.me' WHERE cust_id = 30001;
