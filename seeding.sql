eaf7d2b85842f9f57f733a9298e9378536d5b52f
Joaquin Albors <joaquin.albors01@gmail.com>
2024-07-23 01:20:23 +0200

    Update seeding.sql

--- seeding.sql
+++ seeding.sql
@@ -1,11 +1,11 @@
 -- Active: 1721290976752@@127.0.0.1@3306@dealership
-INSERT INTO cars (vin, manufacturer, model, year_car, color)
-VALUES ('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
-       ('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
-       ('RKXVNNIHLVVZOUB4M',	'Ford',  'Fusion',	2018, 'White'),
-       ('HKNDGS7CU31E9Z7JW',	'Toyota',	'RAV4',	2018,	'Silver'),
-       ('DAM41UDN3CHU2WVF6',	'Volvo',	'V60',	2019,	'Gray'),
-       ('DAM41UDN3CHU2WVF6',	'Volvo', 'V60 Cross Country',	2019,	'Gray');
+INSERT INTO cars (id, vin, manufacturer, model, car_year, color)
+VALUES (1,'3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+       (2,'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+       (3,'RKXVNNIHLVVZOUB4M',	'Ford',  'Fusion',	2018, 'White'),
+       (4,'HKNDGS7CU31E9Z7JW',	'Toyota',	'RAV4',	2018,	'Silver'),
+       (5,'DAM41UDN3CHU2WVF6',	'Volvo',	'V60',	2019,	'Gray'),
+       (6,'DAM41UDN3CHU2WVF6',	'Volvo', 'V60 Cross Country',	2019,	'Gray');
 
 INSERT INTO customers (customer_ID,	name, phone, email,	address_cust, city,	state_province,	country, zip_code)
 VALUES (10001, 'Pablo Picasso', '+34 636 17 63 82', NULL, 'Paseo de la Chopera, 14',	'Madrid',	'Madrid',	'Spain',	'28045'),
@@ -17,7 +17,7 @@ VALUES ('00001',	'Petey Cruiser',	'Madrid'),
        ('00002',	'Anna Sthesia',	'Barcelona'),
        ('00003',	'Paul Molive',	'Berlin'),
        ('00004',	'Gail Forcewind',	'Paris'),
-       ('00005',	'Paige Turner', 'Mimia'),	
+       ('00005',	'Paige Turner',      'Mimia'),	
        ('00006',	'Bob Frapples',	'Mexico City'),
        ('00007',	'Walter Melon',	'Amsterdam'),	
        ('00008',	'Shonda Leer',	'São Paulo');
