3799def1cfdd1f88a2dc2f7c068a5f2258456b64
Joaquin Albors <joaquin.albors01@gmail.com>
2024-07-23 00:25:36 +0200

    Update.sql

--- update.sql
+++ update.sql
@@ -1,11 +1,9 @@
 -- Active: 1721290976752@@127.0.0.1@3306@dealership
-SET SQL_SAFE_UPDATES = 0;
+UPDATE customers
+SET cemail = 'ppicasso@gmail.com' WHERE customer_ID = 10001;
 
 UPDATE customers
-SET cust_email = 'ppicasso@gmail.com' WHERE cust_id = 10001;
+SET email = 'lincoln@us.gov' WHERE customer_ID = 20001;
 
 UPDATE customers
-SET cust_email = 'lincoln@us.gov' WHERE cust_id = 20001;
-
-UPDATE customers
-SET cust_email = 'hello@napoleon.me' WHERE cust_id = 30001;
+SET email = 'hello@napoleon.me' WHERE customer_ID = 30001;
