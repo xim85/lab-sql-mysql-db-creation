f2e7dd13c4ac37391d2534d576d9e5c280793686
Joaquin Albors <joaquin.albors01@gmail.com>
2024-07-23 00:32:33 +0200

    seeding.sql

--- seeding.sql
+++ seeding.sql
@@ -8,13 +8,13 @@ VALUES ('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
        ('DAM41UDN3CHU2WVF6',	'Volvo', 'V60 Cross Country',	2019,	'Gray');
 
 INSERT INTO customers (customer_ID,	name, phone, email,	address_cust, city,	state_province,	country, zip_code)
-VALUES ('10001', 'Pablo Picasso', '+34 636 17 63 82', NULL, 'Paseo de la Chopera, 14',	'Madrid',	'Madrid',	'Spain',	'28045'),
-       ('20001', 'Abraham Lincoln',	'+1 305 907 7086',	NULL, '120 SW 8th St',	'Miami',	'Florida',	'United States',	'33130'),
-       ('30001', 'Napoléon Bonaparte',	'+33 1 79 75 40 00', NULL, '40 Rue du Colisée',	'Paris', 'Île-de-France', 'France',	'75008');
+VALUES (10001, 'Pablo Picasso', '+34 636 17 63 82', NULL, 'Paseo de la Chopera, 14',	'Madrid',	'Madrid',	'Spain',	'28045'),
+       (20001, 'Abraham Lincoln',	'+1 305 907 7086',	NULL, '120 SW 8th St',	'Miami',	'Florida',	'United States',	'33130'),
+       (30001, 'Napoléon Bonaparte',	'+33 1 79 75 40 00', NULL, '40 Rue du Colisée',	'Paris', 'Île-de-France', 'France',	'75008');
 
 INSERT INTO salespersons (staff_ID,	name,	store)
 VALUES ('00001',	'Petey Cruiser',	'Madrid'),
-	   ('00002',	'Anna Sthesia',	'Barcelona'),
+       ('00002',	'Anna Sthesia',	'Barcelona'),
        ('00003',	'Paul Molive',	'Berlin'),
        ('00004',	'Gail Forcewind',	'Paris'),
        ('00005',	'Paige Turner', 'Mimia'),	
